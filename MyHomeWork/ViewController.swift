ef09bdad6c670196f9c4b191b4e46c6c05f7a92c
vicleecl <vic@mail.pch.org.tw>
2017-12-16 20:27:46 +0800

    Initial Commit

---
+++ MyHomeWork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyHomeWork
+//
+//  Created by 李 志良 on 16/12/2017.
+//  Copyright © 2017 李 志良. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
